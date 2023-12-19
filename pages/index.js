f2bdb311dfa1c71baf3c0fb382f70094788d0c1c
narendrarty17 <narendrarty17@gmail.com>
2023-12-19 09:51:24 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mr-0">
       <Navigation />
       <DeveloperIntro />
       <MyServices />
